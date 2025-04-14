278b48a472472fe857dd3c9adfe836878e8436ea
Aryan Prajapati <aryangithub2017@gmail.com>
2025-04-15 04:26:16 +0530

    commit
    commit

--- Clint/run.py
+++ Clint/run.py
@@ -1,5 +1,14 @@
-from prediction_using_opencv import video_streaming, set_video_path, set_user_id
+from prediction_using_opencv import AutoCheckoutSystem
 
-set_video_path("video1.mp4")
-set_user_id("aryangithub2017")
-video_streaming()
+# Instantiate the predictor class
+client = AutoCheckoutSystem()
+
+# Set configurations
+url = "https://autocheckouts.onrender.com/add_item"  # Or your actual deployed URL
+client.set_end_point_url(url)
+client.set_time_out_threshold(20)
+client.set_video_path("video1.mp4")
+client.set_user_id("id")
+
+# Run video streaming
+client.video_streaming()
